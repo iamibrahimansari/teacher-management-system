2b286a7fa0834022989ec5b7e935c08c4e2130c1
Ibrahim Ansari <iamibrahimansari@gmail.com>
2023-09-04 15:17:32 +0530

    fix error in controller function

--- server/controllers/teacher.Controller.js
+++ server/controllers/teacher.Controller.js
@@ -39,8 +39,9 @@ const updateTeacher = async (req, res) =>{
         return res.status(404).json({error: 'User doesn\'t exist'})
     }
 
+    let teacher = null;
     try{
-        const teacher = await Teacher.findByIdAndUpdate(id, {...req.body}, {new: true});
+        teacher = await Teacher.findByIdAndUpdate(id, {...req.body}, {new: true});
     }catch(error){
         return res.json('Your updated cellphone or email is associated with other exist teacher');
     }
